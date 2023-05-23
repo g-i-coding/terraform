180961c3105339506d19b6178082006fcbdcf29f
Cody Elzey <g.i.coding@Codys-MacBook-Pro.local>
2023-05-23 08:10:35 -0500

    adding git ignore

--- vpc.tf
+++ vpc.tf
@@ -100,7 +100,7 @@ resource "aws_route_table" "private-rt" {
 resource "aws_route" "private" {
   route_table_id         = aws_route_table.private-rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_nat_gateway.main_nat_gw.id
+  nat_gateway_id         = aws_nat_gateway.main_nat_gw.id
 }
 resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private-rt.id
